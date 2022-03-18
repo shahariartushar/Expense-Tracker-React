70878a09d8870b1dadcd2a615bf97700f5784d42
shahariartushar <shahariartushar@gmail.com>
2022-03-18 17:52:01 +0100

    add expense components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import Expenses from "./components/Expenses";
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Health Insurance",
+      amount: 110.5,
+      date: new Date(2022, 2, 15),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 110.5,
+      date: new Date(2022, 2, 15),
+    },
+    {
+      id: "e3",
+      title: "Education Insurance",
+      amount: 110.5,
+      date: new Date(2022, 2, 15),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
